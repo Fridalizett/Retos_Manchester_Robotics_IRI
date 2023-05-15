0f1f6c5c40d8f54745680b45f46df625decd9dfd
J3z4r3l <A01735226@itesm.mx>
2023-05-15 02:22:01 -0600

    commit

--- Week_3/mini_c3/scripts/imagen.py
+++ Week_3/mini_c3/scripts/imagen.py
@@ -1,47 +1,102 @@
 #!/usr/bin/env python
-from __future__ import print_function
- 
-import sys
-import rospy
+
+from ast import Pass
 import cv2
-from std_msgs.msg import String
+import numpy as np
+import rospy
+import cv_bridge
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
 
-class image_converter:
+class procesamiento:
+    def __init__(self):
+        self.ancho = 640
+        self.alto = 320
+        
+        self.image = None	
+        self.bridge = cv_bridge.CvBridge()
+        self.green_time = 0
+        self.red_time = 0
+        self.yellow_time = 0
+	#Definir suscriptores y publicadores
+        self.image_sub = rospy.Subscriber('/video_source/raw',Image, self.image_callback) 
 
-  def __init__(self):
-    self.image_pub = rospy.Publisher("image_topic_2",Image)
+        self.image_pub = rospy.Publisher('/procesada',Image, queue_size=10)
 
-    self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber("image_topic",Image,self.callback)
+    def image_callback(self,msg):
+        self.image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
 
-  def callback(self,data):
-    try:
-      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-    except CvBridgeError as e:
-      print(e)
+    def preprocesamiento(self,image):
+        rotar = cv2.rotate(image,cv2.ROTATE_180)
+        redimensionar = cv2.resize(rotar,(self.ancho, self.alto))
+        src_gray = cv2.cvtColor(redimensionar, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.GaussianBlur(src_gray, (19, 19), 0)
+        return blurred
+    def detect_traffic_light(self,image):
+        rotar = cv2.rotate(image,cv2.ROTATE_180)
+        redimensionar = cv2.resize(rotar,(self.ancho, self.alto))
+        frame = np.copy(redimensionar)  # Copiar la imagen original
+        #frame = self.image  # Utilizar la imagen almacenada en self.image
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.medianBlur(gray, 5)
+        circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 20,
+                               param1=50, param2=30,
+                               minRadius=50, maxRadius=70)
+        if circles is not None:
+            circles = np.uint16(np.around(circles))
+            for i in circles[0,:]:
+                color = frame[i[1], i[0]]
+                if color[2] > 100 and color[1] < 100 and color[0] < 100:
+                    cv2.circle(frame,(i[0],i[1]),i[2],(0,0,255),2)
+                    cv2.putText(frame, 'rojo', (i[0], i[1]), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), lineType=cv2.LINE_AA)
+                    self.yellow_time = 0
+                    self.green_time = 0
+                    self.red_time += 1
+                    if self.red_time > 2*30: # 2 segundos a una frecuencia de 30 fps
+                        print("1")
+                elif color[2] > 100 and color[1] > 100 and color[0] < 100:
+                    cv2.circle(frame,(i[0],i[1]),i[2],(0,255,255),2)
+                    cv2.putText(frame, 'amarillo', (i[0], i[1]), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 255), lineType=cv2.LINE_AA)
+                    self.yellow_time += 1
+                    if self.yellow_time > 2*30: # 2 segundos a una frecuencia de 30 fps
+                        print("2")
+                        self.red_time = 0
+                        self.green_time = 0
+                elif color[1] > color[0] and color[1] > color[2] and color[0] < 100 and color[2] < 100:
+                    cv2.circle(frame,(i[0],i[1]),i[2],(0,255,0),2)
+                    cv2.putText(frame, 'verde', (i[0], i[1]), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), lineType=cv2.LINE_AA)
+                    self.green_time += 1
+                    if self.green_time > 2*30: # 2 segundos a una frecuencia de 30 fps
+                        print("3")
+                else:
+                    pass
+        cv2.imshow('Circulos detectados', frame)
+        cv2.waitKey(1)
+        return frame
+    def run(self):
+        if self.image is None:
+           return
+        detection = self.detect_traffic_light(self.image)
+        if detection is not None:
+            ros_img = self.bridge.cv2_to_imgmsg(detection)
+            self.image_pub.publish(ros_img)
+    #def run(self):
+     #   if self.image is None:
+      #      return
+       # #procesada = self.preprocesamiento(self.image)
+        #detection=self.detect_traffic_light(self.image)
+        #ros_img = self.bridge.cv2_to_imgmsg(procesada)
+        #ros_img = self.bridge.cv2_to_imgmsg(detection)
+        #self.image_pub.publish(ros_img)
 
-    (rows,cols,channels) = cv_image.shape
-    if cols > 60 and rows > 60 :
-      cv2.circle(cv_image, (50,50), 10, 255)
 
-    cv2.imshow("Image window", cv_image)
-    cv2.waitKey(3)
 
-    try:
-      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
-    except CvBridgeError as e:
-      print(e)
-
-def main(args):
-  ic = image_converter()
-  rospy.init_node('image_converter', anonymous=True)
-  try:
-    rospy.spin()
-  except KeyboardInterrupt:
-    print("Shutting down")
-  cv2.destroyAllWindows()
-
-if __name__ == '__main__':
-    main(sys.argv)
+if __name__ == "__main__":
+    image_proc = procesamiento()
+    rospy.init_node("procesamiento")
+    loop_rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        try:
+            image_proc.run()
+            loop_rate.sleep()
+        except rospy.ROSInterruptException:
+            pass
